9eedd94579d84cafc8edbfca4fa0d7cc45f05c75
Joy N Patel <joypatel242001@gmail.com>
2022-04-14 18:36:28 +0530

    added updated SETTING

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -132,5 +132,7 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
