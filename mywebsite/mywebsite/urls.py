13d921db5cc01f621113ccc094c4deeb4600956d
khushal0411 <khushalgoyal780@gmail.com>
2022-04-14 17:56:57 +0530

    K

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path,include
+from django.urls import path, include
 
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('events.urls')),
-    path('members/',include('django.contrib.auth.urls')),
-    path('members/',include('members.urls')),
+    path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
