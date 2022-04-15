8f0b84a0ab8f7ed4c43bf9bc173c895ae6d53df5
khushal0411 <khushalgoyal780@gmail.com>
2022-04-15 21:23:01 +0530

    changes

--- mywebsite/mywebsite/wsgi.py
+++ mywebsite/mywebsite/wsgi.py
@@ -17,6 +17,7 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mywebsite.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
 application = get_wsgi_application()
